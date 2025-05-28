04607872c86602acf5970df77b993699848d1ce7
Victor Bruce <victorbruce82@gmail.com>
2025-05-28 16:08:24 +0000

    feat: setup typescript with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,20 @@
 const path = require("path");
 
 module.exports = {
-	entry: './src/index.js',
-	output: {
-		filename: 'main.js',
-		path: path.resolve(__dirname, 'dist')
-	}
-}
+  entry: "./src/index.ts",
+  devtool: "inline-source-map",
+  module: {
+    rules: [
+      {
+        test: "/.ts$/",
+        use: "ts-loader",
+        include: [path.resolve(__dirname, "src")],
+        exclude: "/node_modules/",
+      },
+    ],
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+};

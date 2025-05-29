a882eef21b270dc270fe04ce28acb53a59aaf73d
Victor Bruce <victorbruce82@gmail.com>
2025-05-29 09:42:02 +0000

    feat: add a basic ui testing out css and static images

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,3 @@
-import * as _ from 'lodash';
+import './scss/style.scss';
 
-function component() {
-  const element = document.createElement("div");
-
-  element.innerHTML = _.join(["Hello", "webpack"], " ");
-
-  return element;
-}
-
-document.body.appendChild(component());
+console.log("webpack is running but");

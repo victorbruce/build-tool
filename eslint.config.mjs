c0835aeb6c4c7a46d3a790fc8ea68cf2cf1dcc18
Victor Bruce <victorbruce82@gmail.com>
2025-05-29 10:48:36 +0000

    feat: add prettier to format code and also integrate with eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,27 +1,28 @@
-import js from "@eslint/js";
-import globals from "globals";
-import tseslint from "typescript-eslint";
-import json from "@eslint/json";
-import { defineConfig } from "eslint/config";
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import json from '@eslint/json';
+import { defineConfig } from 'eslint/config';
 
 export default defineConfig([
   {
-    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
-    plugins: { js },
-    extends: ["js/recommended"],
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
+    plugins: { js, prettier: require('eslint-plugin-prettier') },
+    extends: ['js/recommended', 'plugin:prettier/recommended'],
   },
   {
-    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
     languageOptions: { globals: globals.browser },
     rules: {
-      "no-console": ["error", { allow: ["log", "debug", "info"] }],
+      'no-console': ['error', { allow: ['log', 'debug', 'info'] }],
+      'prettier/prettier': 'error',
     },
   },
   tseslint.configs.recommended,
   {
-    files: ["**/*.json"],
+    files: ['**/*.json'],
     plugins: { json },
-    language: "json/json",
-    extends: ["json/recommended"],
+    language: 'json/json',
+    extends: ['json/recommended'],
   },
 ]);
